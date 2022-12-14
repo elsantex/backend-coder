4d74189b5b32d1f5c646c71363cf78d992892b5f
elsantex <santiago.grosso4@gmail.com>
2022-12-13 21:44:09 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+class ProductManager{
+    products
+    static codeInc = 0
+    constructor(){
+        this.products=[]
+    }
+    addProduct(title, description, price, thumbnail, code, stock){
+        ProductManager.codeInc++
+        const product ={
+            title,
+            description,
+            price,
+            thumbnail,
+            code: ProductManager.codeInc,
+            stock
+        }
+
+        // if(){
+
+        // }
+    }
+    getProducts(){
+        return this.products
+    }
+    getProductById(){
+        //retornar el producto que cuente con este id
+        //utilizar find()
+    }
+}
+
+
+const productManager = new ProductManager() 
+productManager.addProduct("producto prueba", "este es un producto prueba", 200, "Sin imagen",undefined,25)
