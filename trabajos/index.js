d8996f28a230c3ddd85931118c7c73573a18b210
elsantex <santiago.grosso4@gmail.com>
2022-12-26 16:52:26 -0300

    progreso 2do trabajo

--- trabajos/index.js
+++ trabajos/index.js
@@ -1,14 +1,19 @@
+const fs = require("fs")
+
+
 const productsArray = []
 
-class ProductManager{
+class ProductManager{   
     static codeInc = 1
-    constructor(title, description, price, thumbnail,  stock){
+    constructor(path, title, description, price, thumbnail,stock){
+        this.path = path
         this.title = title
          this.description = description
          this. price = price
          this.thumbnail = thumbnail
          this.stock = stock
          ProductManager.codeInc
+         this.loadFile()
     }
     addProduct(){
         const product ={
@@ -19,69 +24,71 @@ class ProductManager{
             stock: this.stock,
             code: ProductManager.codeInc++
         }
-    
-    productsArray.push(product)
 
-    //    if(product){
-    //     this.products.find(producto => product.code === producto.code)
+    //      if(product){
+    //     productsArray.find(producto => product.code === producto.code)
     //     console.log("el codigo ya existe")
+    //    }else{   
     //    }
+        
+    productsArray.push(product)
+    this.saveFile()
+
+      
     }
     getProducts(){
-        console.log(productsArray)
+        if(fs.existsSync("./productos.json")){
+           console.log(JSON.parse(fs.readFileSync("./productos.json", "utf-8"))) 
+        }else{
+            console.log(productsArray)
+        }
+        
     }
-    // getProductById(id){
-    //     if(this.products.find(id => this.product.code === id)){
-    //     return console.log(id)
-    //     }
-    // }
+
+    getProductById(number){
+       let itemFs
+        if (fs.existsSync("./productos.json")) {
+            if(itemFs !== Object){
+           itemFs = JSON.parse(fs.readFileSync("./productos.json", "utf-8"))
+            }
+        let item = itemFs.find(itemFs => itemFs.code === number)
+          console.log(item)
+        } else {
+            let item = productsArray.find(item => item.code === number)
+      console.log(item)
+        }
+    }
+
+    saveFile(){
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(productsArray))
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    loadFile(){
+        try {
+            let item = JSON.parse(fs.readFileSync(this.path, "utf-8"))
+            productsArray.push(item)
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
 }
 
 
-const productManager = new ProductManager("producto prueba","este es un producto prueba", 200, "Sin imagen", 25) 
-const productManager2 = new ProductManager("producto prueba2","este es un producto prueba", 300, "Sin imagen", 25) 
-const productManager3 = new ProductManager("producto prueba3","este es un producto prueba", 400, "Sin imagen", 25) 
-const productManager4 = new ProductManager("producto prueba4","este es un producto prueba", 500, "Sin imagen", 25) 
-productManager.addProduct()
-productManager2.addProduct()
-productManager3.addProduct()
-productManager4.addProduct()
+const productManager = new ProductManager("./productos.json","producto prueba","este es un producto prueba", 200, "Sin imagen", 25) 
+// const productManager2 = new ProductManager("./productos.json","producto prueba2","este es un producto prueba", 300, "Sin imagen", 35) 
+// const productManager3 = new ProductManager("./productos.json","producto prueba3","este es un producto prueba", 400, "Sin imagen", 45) 
+// const productManager4 = new ProductManager("./productos.json","producto prueba4","este es un producto prueba", 500, "Sin imagen", 55) 
+// productManager.addProduct()
+// productManager2.addProduct()
+// productManager3.addProduct()
+// productManager4.addProduct()
+
+
 
 productManager.getProducts()
-
-
-// ///// ejemplo ////
-
-// const arrayPersonas2 = []
-
-// class Personas{
-//     static id = 1
-//     constructor(nombre, apellido, edad){
-//         this.name = nombre
-//         this.surname = apellido
-//         this.age = edad
-//         Personas.id
-//     }
-
-//     GuardarPersona(){
-//         const persona = {
-//             nombre: this.name,
-//             apellido: this.surname,
-//             edad: this.age,
-//             id: Personas.id++
-//         }
-//     arrayPersonas2.push(persona)
-//     }
-//     getPersona(){
-//         console.log(arrayPersonas2)
-//     }
-// }
-// const persona1 = new Personas("Santiago", "Grosso", 21)
-// const persona2 = new Personas("Carlos", "Baute", 33)
-// const persona3 = new Personas("Juan", "Quiroz", 75)
-
-// persona1.GuardarPersona()
-// persona2.GuardarPersona()
-// persona3.GuardarPersona()
-
-// persona2.getPersona()
+productManager.getProductById(2)
